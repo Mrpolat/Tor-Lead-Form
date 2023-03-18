8ff831e877079c81f74c95f1a20f71214da62926
Muhsin Polat Uçar <m.polat.ucar@gmail.com>
2023-03-18 03:50:22 +0300

    Update api.js

--- Front-end/src/api/api.js
+++ Front-end/src/api/api.js
@@ -1,13 +1,13 @@
 import axios from "axios"; 
 
-//aşağıda ki fonksiyona dışarıdan bir telefon numarasını parametre olarak alıp url de istenilen yere (+14 ile başlayan bölüm) ekledikten sonra
+//aşağıda ki fonksiyona dışarıdan bir telefon numarasını parametre olarak alıp url de istenilen yere ekledikten sonra
 //endpointten dönen response'un içerisinde, "response.valid= " in karşısında yazan boolean değere bakarak girilen telefon numarasına Validation işlemi yapılabilir.
 //ben api kullanmadan aynı işlemi gerçekleştirdim.
-<<<<<<< Updated upstream
-//leadForm component içerisinde console basması için 91. satırda bu kodu çalıştırdım.
-=======
-//leadForm component içerisinde console basması için 91. satırda bu kodu çalıştırdım..
->>>>>>> Stashed changes
+
+
+//leadForm component içerisinde console basması için 91. satırda bu isteği çalıştırdım.
+//leadForm component içerisinde console basması için 91. satırda bu isteği çalıştırdım..
+
 
 export const phoneNumberCheck = (phoneNumber) => {
     axios.get("http://localhost:3001/twilio/"+phoneNumber).then((response)=>console.log(response));
